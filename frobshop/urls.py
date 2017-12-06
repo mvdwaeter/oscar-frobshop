0ae3f566b54124349c22bd84fc8fb5331f109680
Kristin Brandenburg <kristinburg@gmail.com>
2017-12-06 19:23:59 +0100

    updated media settings

--- frobshop/urls.py
+++ frobshop/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from oscar.app import application
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^i18n/', include('django.conf.urls.i18n')),
@@ -25,4 +27,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'', include(application.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
